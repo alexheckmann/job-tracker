7571e8893430742c2e34af454dde81c08b7b6305
Alex Heckmann <ah@ecommunications.de>
2024-04-04 21:50:04 +0200

    feat(add job retrieval and posting api)

---
+++ app/api/v1/jobs/route.ts
@@ -0,0 +1,29 @@
+import {getJobs, insertJob} from "@/lib/db/db";
+import {NextRequest, NextResponse} from "next/server";
+import {HttpStatusCode} from "axios";
+import {insertJobSchema} from "@/lib/db/schema";
+
+export const dynamic = 'force-dynamic'
+export const runtime = 'nodejs'
+
+export async function GET() {
+    try {
+        const results = await getJobs()
+        return NextResponse.json({jobs: results}, {status: HttpStatusCode.Ok})
+    } catch (error) {
+        return NextResponse.json({error}, {status: HttpStatusCode.InternalServerError})
+    }
+}
+
+export async function POST(req: NextRequest) {
+    const unvalidatedJob = await req.json()
+
+    const validatedJob = insertJobSchema.parse(unvalidatedJob)
+
+    try {
+        const createdJob = await insertJob(validatedJob)
+        return NextResponse.json(createdJob[0], {status: HttpStatusCode.Created})
+    } catch (error) {
+        return NextResponse.json({error}, {status: HttpStatusCode.InternalServerError})
+    }
+}
