a3eb40ba80ea464f89b78455504e5f610602b2c7
Alex Heckmann <ah@ecommunications.de>
2024-04-05 20:47:39 +0200

    fix(correct color schemes for status badge)

--- app/components/status-badge.tsx
+++ app/components/status-badge.tsx
@@ -1,11 +1,14 @@
 import {ApplicationStatus} from "@/lib/db/schema";
 import {Badge} from "@/components/ui/badge";
+import {cn} from "@/lib/utils";
 
 interface StatusBadgeProps {
-    status: typeof ApplicationStatus["_type"][number]
+    status: typeof ApplicationStatus["_type"][number],
+    children?: any,
+    className?: string
 }
 
-export default function StatusBadge({status}: StatusBadgeProps) {
+export default function StatusBadge({status, children, className}: StatusBadgeProps) {
 
     let statusColor;
     switch (status) {
@@ -15,18 +18,24 @@ export default function StatusBadge({status}: StatusBadgeProps) {
         case "Applied":
             statusColor = "bg-blue-100 text-blue-800 hover:bg-blue-300 hover:text-blue-900";
             break;
-        case "Interview":
-            statusColor = "bg-green-100 text-green-800 hover:bg-green-300 hover:text-green-900";
+        case "Interviewed":
+            statusColor = "bg-yellow-100 text-yellow-800 hover:bg-yellow-300 hover:text-yellow-900";
             break;
         case "Declined":
             statusColor = "bg-red-100 text-red-800 hover:bg-red-300 hover:text-red-900";
+            break;
+        case "Offered":
+            statusColor = "bg-green-100 text-green-800 hover:bg-green-300 hover:text-green-900";
             break;
         default:
             statusColor = "bg-yellow-100 text-yellow-800 hover:bg-yellow-300 hover:text-yellow-900";
     }
 
     return (
-        <Badge className={statusColor}>{status}</Badge>
+        <Badge className={cn(statusColor, className, "justify-between")}>
+            {status}
+            {children}
+        </Badge>
     )
 
 }
