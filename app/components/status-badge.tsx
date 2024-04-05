bc9f8ba9adbcd8a9cd44d9d4b6158b08f6c1e40d
Alex Heckmann <ah@ecommunications.de>
2024-04-05 11:45:24 +0200

    refactor(increase conditional styling readability for status badge)

--- app/components/status-badge.tsx
+++ app/components/status-badge.tsx
@@ -7,7 +7,27 @@ interface StatusBadgeProps {
 
 export default function StatusBadge({status}: StatusBadgeProps) {
 
-    const statusColor = status === "Saved" ? "bg-blue-100 text-blue-800" : status === "Applied" ? "bg-yellow-100 text-yellow-800" : status === "Interview" ? "bg-green-100 text-green-800" : status === "Declined" ? "bg-red-100 text-red-800" : "bg-gray-100 text-gray-800"
+    let statusColor;
+    switch (status) {
+        case "Saved":
+            statusColor = "bg-blue-100 text-blue-800";
+            break;
+        case "Applied":
+            statusColor = "bg-yellow-100 text-yellow-800";
+            break;
+        case "Interview":
+            statusColor = "bg-green-100 text-green-800";
+            break;
+        case "Declined":
+            statusColor = "bg-red-100 text-red-800";
+            break;
+        default:
+            statusColor = "bg-gray-100 text-gray-800";
+    }
+    // statusColor as switch statement
+    // switch (status) {
+    //     case "Saved":
+
 
     return (
         <Badge className={statusColor}>{status}</Badge>
