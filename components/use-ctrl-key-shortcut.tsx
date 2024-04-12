0c80770027efe2124feb234eb129128056bd5344
Alex Heckmann <ah@ecommunications.de>
2024-04-12 10:49:19 +0200

    refactor(extract shortcut key into custom hook)

---
+++ components/use-ctrl-key-shortcut.tsx
@@ -0,0 +1,26 @@
+import {useEffect} from "react";
+
+
+type ValidKey = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm'
+    | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'
+    | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+    | 'F1' | 'F2' | 'F3' | 'F4' | 'F5' | 'F6' | 'F7' | 'F8' | 'F9' | 'F10' | 'F11' | 'F12'
+    | 'ArrowUp' | 'ArrowDown' | 'ArrowLeft' | 'ArrowRight'
+    | 'Tab' | 'Shift' | 'Control' | 'Alt' | 'CapsLock' | 'Space' | 'Enter' | 'Backspace' | 'Delete';
+
+export function useCtrlKeyShortcut(key: ValidKey, keypressEvent: () => void) {
+    useEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+            console.log(event.key)
+            if (event.key === key && event.ctrlKey) {
+                keypressEvent()
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+}
