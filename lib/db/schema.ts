029ddfc7f2aa984a061c124c5d3946e3970d980a
Alex Heckmann <ah@ecommunications.de>
2024-04-17 10:57:36 +0200

    refactor(change most data types to varchar to reduce exploitability)

--- lib/db/schema.ts
+++ lib/db/schema.ts
@@ -1,4 +1,4 @@
-import {boolean, pgEnum, pgTable, serial, text, timestamp,} from 'drizzle-orm/pg-core'
+import {boolean, pgEnum, pgTable, serial, timestamp, varchar} from 'drizzle-orm/pg-core'
 import {z} from "zod";
 import {createInsertSchema, createSelectSchema} from "drizzle-zod";
 
@@ -17,70 +17,99 @@ const statusEnum = pgEnum("applicationStatus", [
 
 export const ApplicationStatus = z.enum(statusEnum.enumValues)
 
+const jobsMaxLengthConstraints = {
+    role: 128,
+    company: 128,
+    exactTitle: 128,
+    location: 256,
+    link: 256,
+    salary: 16,
+    notes: 2048
+}
+
 export const JobsTable = pgTable(
     'jobs',
     {
         id: serial('id').primaryKey(),
-        role: text('role').notNull(),
-        company: text('company').notNull(),
-        status: text('applicationStatus', {enum: statusEnum.enumValues}).notNull(),
-        exactTitle: text('exact_title').notNull(),
-        location: text('location').notNull(),
-        link: text('link'),
-        salary: text('salary'),
+        role: varchar('role', {length: jobsMaxLengthConstraints.role}).notNull(),
+        company: varchar('company', {length: jobsMaxLengthConstraints.company}).notNull(),
+        status: varchar('applicationStatus', {enum: statusEnum.enumValues}).notNull(),
+        exactTitle: varchar('exact_title', {length: jobsMaxLengthConstraints.exactTitle}).notNull(),
+        location: varchar('location', {length: jobsMaxLengthConstraints.location}).notNull(),
+        link: varchar('link', {length: jobsMaxLengthConstraints.link}),
+        salary: varchar('salary', {length: jobsMaxLengthConstraints.salary}),
         isFavorite: boolean('is_favorite').notNull(),
         isRecruiter: boolean('is_recruiter').notNull(),
-        notes: text('notes'),
+        notes: varchar('notes', {length: jobsMaxLengthConstraints.notes}),
         lastUpdate: timestamp('last_update').defaultNow().notNull(),
     }
 )
 
 export const insertJobSchema = createInsertSchema(JobsTable, {
-    role: z.string().min(1),
-    company: z.string().min(1),
-    location: z.string().min(1),
+    role: z.string().min(1).max(jobsMaxLengthConstraints.role),
+    company: z.string().min(1).max(jobsMaxLengthConstraints.company),
+    location: z.string().min(1).max(jobsMaxLengthConstraints.location),
+    exactTitle: z.string().max(jobsMaxLengthConstraints.exactTitle),
+    link: z.string().max(jobsMaxLengthConstraints.link),
+    salary: z.string().max(jobsMaxLengthConstraints.salary),
+    notes: z.string().max(jobsMaxLengthConstraints.notes),
     lastUpdate: z.coerce.date()
 })
 
 export const selectJobSchema = createSelectSchema(JobsTable, {
-    role: z.string().min(1),
-    company: z.string().min(1),
-    location: z.string().min(1),
+    role: z.string().min(1).max(jobsMaxLengthConstraints.role),
+    company: z.string().min(1).max(jobsMaxLengthConstraints.company),
+    location: z.string().min(1).max(jobsMaxLengthConstraints.location),
+    exactTitle: z.string().max(jobsMaxLengthConstraints.exactTitle),
+    link: z.string().max(jobsMaxLengthConstraints.link),
+    salary: z.string().max(jobsMaxLengthConstraints.salary),
+    notes: z.string().max(jobsMaxLengthConstraints.notes),
     lastUpdate: z.coerce.date()
 })
 export type JobEntry = z.infer<typeof insertJobSchema>
 
 export type InsertedJobEntry = z.infer<typeof selectJobSchema>
 
+const contactsMaxLengthConstraints = {
+    name: 128,
+    role: 128,
+    company: 128,
+    email: 256,
+    phone: 32,
+    linkedin: 256,
+    location: 256,
+    notes: 2048
+}
+
 export const ContactsTable = pgTable(
     'contacts',
     {
         id: serial('id').primaryKey(),
-        name: text('name').notNull(),
-        role: text('role').notNull(),
-        company: text('company').notNull(),
-        email: text('email'),
-        phone: text('phone'),
-        linkedin: text('linkedin'),
-        location: text('location').notNull(),
-        notes: text('notes'),
+        name: varchar('name', {length: contactsMaxLengthConstraints.name}).notNull(),
+        role: varchar('role', {length: contactsMaxLengthConstraints.role}).notNull(),
+        company: varchar('company', {length: contactsMaxLengthConstraints.company}).notNull(),
+        email: varchar('email', {length: contactsMaxLengthConstraints.email}),
+        phone: varchar('phone', {length: contactsMaxLengthConstraints.phone}),
+        linkedin: varchar('linkedin', {length: contactsMaxLengthConstraints.linkedin}),
+        location: varchar('location', {length: contactsMaxLengthConstraints.location}).notNull(),
+        notes: varchar('notes', {length: contactsMaxLengthConstraints.notes}),
         lastUpdate: timestamp('last_update').defaultNow().notNull(),
     }
 )
 
 export const insertContactSchema = createInsertSchema(ContactsTable, {
-    name: z.string().min(1),
-    role: z.string().min(1),
-    company: z.string().min(1),
-    location: z.string().min(1),
+    name: z.string().min(1).max(contactsMaxLengthConstraints.name),
+    role: z.string().min(1).max(contactsMaxLengthConstraints.role),
+    company: z.string().min(1).max(contactsMaxLengthConstraints.company),
+    location: z.string().min(1).max(contactsMaxLengthConstraints.location),
     lastUpdate: z.coerce.date()
 })
 
 export const selectContactSchema = createSelectSchema(ContactsTable, {
-    name: z.string().min(1),
-    role: z.string().min(1),
-    company: z.string().min(1),
-    location: z.string().min(1),
+    name: z.string().min(1).max(contactsMaxLengthConstraints.name),
+    role: z.string().min(1).max(contactsMaxLengthConstraints.role),
+    company: z.string().min(1).max(contactsMaxLengthConstraints.company),
+    location: z.string().min(1).max(contactsMaxLengthConstraints.location),
     lastUpdate: z.coerce.date()
 })
 
