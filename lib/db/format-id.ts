3fe4b9853cffdb1673c58b59c825e27bfe63e98c
Alex Heckmann <ah@ecommunications.de>
2024-04-22 22:10:23 +0200

    refactor(remove unnecessary code)

--- lib/db/format-id.ts
+++
@@ -1,9 +0,0 @@
-import {BSON} from "bson";
-
-/**
- * Format id to BSON ObjectId type so it can be used in queries without errors from MongoDB
- * @param id The id to format.
- */
-export function formatId(id: string) {
-    return new BSON.ObjectId(id);
-}
