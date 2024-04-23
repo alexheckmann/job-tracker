b796e28cedba7f09e7c38cbfd62c934032dc1ef0
Alex Heckmann <ah@ecommunications.de>
2024-04-23 21:20:24 +0200

    style(improve ux of sign in button)

--- components/google-login-button.tsx
+++ components/google-login-button.tsx
@@ -1,16 +1,30 @@
+"use client"
+
 import {Button} from "@/components/ui/button";
 import {signIn} from "next-auth/react";
-import {HTMLAttributes} from "react";
+import {HTMLAttributes, useState} from "react";
 import {cn} from "@/lib/utils";
+import {Loader2} from "lucide-react";
 
 interface GoogleLoginButtonProps extends HTMLAttributes<HTMLButtonElement> {
 
 }
 
 export function GoogleLoginButton({className}: GoogleLoginButtonProps) {
+    const [loading, setLoading] = useState(false)
     return (
-        <Button variant="outline" className={cn(className, "w-fit")} onClick={() => signIn("google")}>
-            Login with Google
+        <Button variant="outline" className={cn(className, "w-fit")} onClick={() => {
+            setLoading(true)
+            signIn("google")
+        }}>
+            {loading ?
+                <div className={"flex flex-row gap-2 items-center"}>
+                    <Loader2 className={"h-4 w-4 animate-spin"}/>
+                    <span className={"animate-pulse"}>Signing in...</span>
+                </div>
+                :
+                "Login with Google"
+            }
         </Button>
     )
 }
