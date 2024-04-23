57790edb87df0f34b295db09d5edd1f408adea3c
Alex Heckmann <ah@ecommunications.de>
2024-04-23 22:13:28 +0200

    style(disable signin button when authenticating)

--- components/google-login-button.tsx
+++ components/google-login-button.tsx
@@ -13,10 +13,14 @@ interface GoogleLoginButtonProps extends HTMLAttributes<HTMLButtonElement> {
 export function GoogleLoginButton({className}: GoogleLoginButtonProps) {
     const [loading, setLoading] = useState(false)
     return (
-        <Button variant="outline" className={cn(className, "w-fit")} onClick={() => {
-            setLoading(true)
-            signIn("google")
-        }}>
+        <Button
+            disabled={loading}
+            variant="outline"
+            className={cn(className, "w-fit")}
+            onClick={() => {
+                setLoading(true)
+                signIn("google")
+            }}>
             {loading ?
                 <div className={"flex flex-row gap-2 items-center"}>
                     <Loader2 className={"h-4 w-4 animate-spin"}/>
