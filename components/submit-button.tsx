3d36bab18be6e1d548ad9f6aaa9acc47305d1990
Alex Heckmann <ah@ecommunications.de>
2024-04-24 12:25:28 +0200

    style(improve loading state feedback)

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -1,10 +1,10 @@
-import {HTMLAttributes, ReactNode} from "react";
+import {HTMLAttributes} from "react";
 import {Button} from "@/components/ui/button";
 import Shortcut from "@/components/shortcut";
 import {Loader2} from "lucide-react";
+import {cn} from "@/lib/utils";
 
 interface SubmitButtonProps extends HTMLAttributes<HTMLButtonElement> {
-    children: ReactNode;
     disabled?: boolean;
 }
 
@@ -12,7 +12,7 @@ export function SubmitButton({children, disabled = false, ...props}: SubmitButto
 
     return (
         <Button {...props} type="submit" className={"gap-2 sm:w-[140px]"} disabled={disabled}>
-            <span>{children}</span>
+            <span className={cn(disabled && "animate-pulse")}>{children}</span>
             {disabled ?
                 <Loader2 className={"h-4 w-4 animate-spin"}/> :
                 <Shortcut keys={["Ctrl", "M"]}/>
