1372327258a6b9a6397767b3c1c1082e524fdf68
Alex Heckmann <ah@ecommunications.de>
2024-05-06 00:08:51 +0200

    fix: correct visibility for mobile settings

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -3,7 +3,7 @@ import {CardContainer} from "@/app/notes/card-container";
 
 export default function SettingsPage() {
     return (
-        <div className="hidden items-start justify-center gap-6 rounded-lg p-8 md:grid lg:grid-cols-2 xl:grid-cols-3">
+        <div className="grid items-start justify-center gap-6 rounded-lg p-8 grid-cols-2 xl:grid-cols-3">
             <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
                 <CardContainer>
                     <RolesCard/>
