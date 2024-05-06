4b17d8e9f8337faa05b45df97be06b119f2497b2
Alex Heckmann <ah@ecommunications.de>
2024-05-06 22:07:00 +0200

    feat: add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,27 @@
+import {GoogleLoginButton} from "@/components/navbar/google-login-button";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import {CardContainer} from "@/app/notes/card-container";
+import Logo from "@/components/logo";
+
+export default function LoginPage() {
+    return (
+        <div className="flex items-center justify-center p-8 h-full">
+            <CardContainer className={"w-[400px]"}>
+                <Card className={"w-full"}>
+                    <CardHeader className={"flex items-center justify-center"}>
+                        <CardTitle className={"flex flex-row flex-wrap flex-grow align-middle gap-2"}>
+                            <Logo className={"w-6 h-6"}/>
+                            Let's get started
+                        </CardTitle>
+                        <CardDescription className={"inline-flex gap-2 items-center text-center pt-6 max-w-[250px]"}>
+                            Join Pegasus, where you can maintain an overview of your job search.
+                        </CardDescription>
+                    </CardHeader>
+                    <CardContent className="flex items-center justify-center">
+                        <GoogleLoginButton/>
+                    </CardContent>
+                </Card>
+            </CardContainer>
+        </div>
+    )
+}
