66757c40d5403d64c947d91f84c69b47a0f1efab
Alex Heckmann <ah@ecommunications.de>
2024-05-19 16:06:25 +0200

    style: improve mobile layout for settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -4,7 +4,7 @@ import {LocationsCard} from "@/app/settings/locations-card";
 
 export default function SettingsPage() {
     return (
-        <div className="grid items-start justify-center gap-6 rounded-lg p-8 grid-cols-2 xl:grid-cols-3">
+        <div className="grid items-start justify-center gap-6 rounded-lg p-2 md:p-8 grid-cols-2 xl:grid-cols-3">
             <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
                 <CardContainer>
                     <RolesCard/>
