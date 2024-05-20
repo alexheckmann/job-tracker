997538e71c66927b953662f968844109bc4f8058
Alex Heckmann <ah@ecommunications.de>
2024-05-20 21:54:32 +0200

    feat: add benefits card component for landing page

---
+++ app/benefits-card.tsx
@@ -0,0 +1,29 @@
+import {Card, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import {Lightbulb} from "lucide-react";
+import {HTMLAttributes} from "react";
+import {cn} from "@/lib/utils";
+
+interface BenefitsCardProps extends HTMLAttributes<HTMLDivElement> {
+
+}
+
+export function BenefitsCard({className}: BenefitsCardProps) {
+    return (
+        <Card
+            className={cn("drop-shadow-xl shadow-black/10", className)}>
+            <CardHeader className="space-y-1 flex md:flex-row justify-start items-start gap-4">
+                <div className="mt-1 bg-primary-foreground p-1 rounded-2xl">
+                    <Lightbulb/>
+                </div>
+                <div>
+                    <CardTitle>
+                        Everything in one place
+                    </CardTitle>
+                    <CardDescription className="text-md mt-2">
+                        Keep your applications, contacts and notes organized to reduce the mental overhead.
+                    </CardDescription>
+                </div>
+            </CardHeader>
+        </Card>
+    );
+}
