f27b62feb2aa9e978d3a21d59a2e26c3955a215f
Alex Heckmann <ah@ecommunications.de>
2024-05-21 14:49:58 +0200

    feat: add thank you page after donating

---
+++ app/thanks/page.tsx
@@ -0,0 +1,9 @@
+export default function ThanksPage() {
+    return (
+        <div className={"text-center w-full"}>
+            <h1 className={"text-4xl font-semibold pt-32"}>
+                Thanks for the donation!
+            </h1>
+        </div>
+    )
+}
