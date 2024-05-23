0e5bcb47b0e128274ed5368708a6b95ac3170ffa
Alex Heckmann <ah@ecommunications.de>
2024-05-23 18:47:23 +0200

    refactor: move put api route for interviews

--- app/api/v1/interviews/route.ts
+++ app/api/v1/interviews/route.ts
@@ -1,4 +1,4 @@
-import {getInterviews, insertInterview} from "@/lib/db/db-helpers";
+import {getInterviews, insertInterview, updateInterview} from "@/lib/db/db-helpers";
 import {NextRequest, NextResponse} from "next/server";
 import {HttpStatusCode} from "axios";
 import mongoose from "mongoose";
@@ -35,3 +35,20 @@ export async function POST(req: NextRequest) {
     }
 }
 
+export async function PUT(req: NextRequest) {
+    const requestedInterview = await req.json().then((data) => ({...data, lastUpdate: new Date(data.lastUpdate)}))
+
+    try {
+
+        const session = await getServerSession(authOptions)
+
+        if (session.id.toString() !== requestedInterview.user.toString()) {
+            return NextResponse.json({error: "Unauthorized"}, {status: HttpStatusCode.Unauthorized})
+        }
+
+        const updatedInterview = await updateInterview(requestedInterview)
+        return NextResponse.json(updatedInterview, {status: HttpStatusCode.Ok});
+    } catch (error) {
+        return NextResponse.json({error}, {status: HttpStatusCode.InternalServerError})
+    }
+}
