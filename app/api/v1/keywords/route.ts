71230a360dddf305f47bc37069bdad0731b3585e
Alex Heckmann <ah@ecommunications.de>
2024-05-24 16:31:55 +0200

    feat: add api routes to add and delete keywords

---
+++ app/api/v1/keywords/route.ts
@@ -0,0 +1,40 @@
+import {NextRequest, NextResponse} from "next/server";
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/app/api/auth/[...nextauth]/authOptions";
+import {getUserById, updateUser} from "@/lib/db/db-helpers";
+import {HttpStatusCode} from "axios";
+
+export async function POST(req: NextRequest) {
+    const newKeywords = await req.json().then((data) => data.value)
+
+    try {
+        const session = await getServerSession(authOptions)
+        const user = await getUserById(session.id)
+        user?.keywords ? user?.keywords.push(newKeywords) : user!.keywords = [newKeywords]
+        await updateUser(session.id, user!)
+        return NextResponse.json(user?.keywords, {status: HttpStatusCode.Created})
+    } catch (error) {
+        return NextResponse.json({error}, {status: HttpStatusCode.InternalServerError})
+    }
+}
+
+export async function DELETE(req: NextRequest) {
+    const keywordsToDelete = await req.json()
+
+    try {
+        const session = await getServerSession(authOptions)
+        const user = await getUserById(session.id)
+
+        const index = user?.keywords?.indexOf(keywordsToDelete)
+        if (index === -1) {
+            return NextResponse.json({error: "Keywords not found"}, {status: HttpStatusCode.NotFound})
+        }
+
+        user?.keywords!.splice(index!, 1)
+
+        await updateUser(session.id, user!)
+        return NextResponse.json(user?.keywords, {status: HttpStatusCode.Ok})
+    } catch (error) {
+        return NextResponse.json({error}, {status: HttpStatusCode.InternalServerError})
+    }
+}
