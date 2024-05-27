390a902408b0e93cd2337381756e08dde1d127a7
Alex Heckmann <ah@ecommunications.de>
2024-05-27 11:43:15 +0200

    perf: remove unnecessary rerender on interview table

--- app/interviews/interview-table.tsx
+++ app/interviews/interview-table.tsx
@@ -24,7 +24,7 @@ export function InterviewTable() {
             setInterviewData(fetchedInterviewData!)
         }
 
-    }, [isJobDataFetched, fetchedInterviewData, session?.user?.locations])
+    }, [isJobDataFetched, fetchedInterviewData])
 
     return (
         <CardContent className={"h-[70dvh] p-5 pt-3 md:p-6"}>
