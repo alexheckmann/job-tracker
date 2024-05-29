62c037df744858e5308c9d6fdf66f067c932507e
Alex Heckmann <ah@ecommunications.de>
2024-05-29 15:49:36 +0200

    style: improve ux of keywords card by hiding add item field

--- app/notes/keywords-card.tsx
+++ app/notes/keywords-card.tsx
@@ -2,7 +2,6 @@
 
 import {useSession} from "next-auth/react";
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
-import {OneInputFieldForm} from "@/components/one-input-field-form";
 import {HTMLAttributes, useEffect, useState} from "react";
 import {InfoHover} from "@/components/info-hover";
 import {RemoveButton} from "@/app/settings/remove-button";
@@ -15,6 +14,7 @@ import {Skeleton} from "@/components/ui/skeleton";
 import {truncateString} from "@/lib/truncate-string";
 import {copyToClipboard} from "@/lib/copy-to-clipboard";
 import {getEmptyArray} from "@/lib/get-empty-array";
+import AddEntryButton from "@/components/add-entry-button";
 
 
 const keywordsCardInfoText = "Save your keywords exactly how you use them on job portals. Some job portals use quotation marks to search for exact phrases."
@@ -75,11 +75,9 @@ export function KeywordsCard() {
                 </CardDescription>
             </CardHeader>
             <CardContent className="grid gap-6">
-                <OneInputFieldForm existingEntries={keywords} formFieldLabel={"Add keywords"}
-                                   submitFunction={submitKeywords} isPendingSubmission={isPendingSubmission}
-                                   disabled={status !== "authenticated"}/>
+
                 <div
-                    className={"flex flex-row flex-wrap gap-2 items-start min-h-[200px] content-start max-h-[240px] overflow-y-auto"}>
+                    className={"flex flex-row flex-wrap gap-2 items-start h-[240px] content-start overflow-y-auto"}>
                     {isLoading ?
                         getEmptyArray(12).map((_, i) => (
                             <Badge key={i} className={`w-[90px]`} variant={"outline"}>
@@ -91,6 +89,9 @@ export function KeywordsCard() {
                                            isPendingRemoval={isPendingRemoval}/>
                         ))}
                 </div>
+                <AddEntryButton existingEntries={keywords} submitFunction={submitKeywords}
+                                isPendingSubmission={isPendingSubmission}
+                                disabled={status !== "authenticated"}/>
             </CardContent>
         </Card>
     )
