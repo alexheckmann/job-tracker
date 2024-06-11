b14561ca6d7c084be11901e76b5c6f8af0923649
Alex Heckmann <ah@ecommunications.de>
2024-06-11 12:58:06 +0200

    feat: add function to transform string properties of an object

---
+++ lib/security/transformObjectStringProperties.ts
@@ -0,0 +1,33 @@
+/**
+ * Transforms the string properties of an object using the provided callback to either encrypt or decrypt the data.
+ * Currently handles the following cases:
+ * - Single string properties
+ * - Array of string properties
+ * - Excluded properties
+ * - Non-string properties
+ *
+ * Note: This function is not recursive and will not transform nested objects.
+ * @param obj The object to transform
+ * @param callback The callback to use to transform the string properties
+ * @param excludeProps The properties to exclude from transformation
+ */
+export function transformObjectStringProperties<T extends Record<string, unknown>>(obj: T, callback: (data: string) => string, excludeProps: string[] = []): T {
+    let transformedObject: T = {} as T;
+
+    for (let prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            if (excludeProps.includes(prop)) {
+                transformedObject[prop] = obj[prop];
+            } else if (typeof obj[prop] === 'string' && obj[prop] !== '') {
+                // @ts-ignore
+                transformedObject[prop] = callback(obj[prop]);
+            } else if (Array.isArray(obj[prop]) && (obj[prop] as any[]).every((item: any) => typeof item === 'string')) {
+                transformedObject[prop] = (obj[prop] as string[]).map((item: string) => callback(item)) as T[Extract<keyof T, string>];
+            } else {
+                transformedObject[prop] = obj[prop];
+            }
+        }
+    }
+
+    return transformedObject;
+}
