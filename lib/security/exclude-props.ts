af522f9190d11fb784ed85b2835a9e70b325f542
Alex Heckmann <ah@ecommunications.de>
2024-06-12 16:00:00 +0200

    feat: add user property to exclude encryption list

--- lib/security/exclude-props.ts
+++ lib/security/exclude-props.ts
@@ -1,4 +1,4 @@
-export const EXCLUDE_OBJECT_PROPS = ["_id", "__v", "createdAt", "updatedAt", "lastUpdate", "date"]
+export const EXCLUDE_OBJECT_PROPS = ["_id", "__v", "createdAt", "updatedAt", "lastUpdate", "date", "user"]
 
 export const EXCLUDE_JOB_PROPS = ["status"]
 
