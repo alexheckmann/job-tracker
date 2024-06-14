e3519573830df8463486eafb55afcb1b211c7c26
Alex Heckmann <ah@ecommunications.de>
2024-06-14 12:29:39 +0200

    fix: exclude user from encryption / decryption to avoid conversion errors

--- lib/security/exclude-props.ts
+++ lib/security/exclude-props.ts
@@ -1,9 +1,9 @@
 export const EXCLUDE_OBJECT_PROPS = ["_id", "__v", "createdAt", "updatedAt", "lastUpdate", "date"]
 
-export const EXCLUDE_JOB_PROPS = ["status"]
+export const EXCLUDE_USER_PROPS = ["encryptedKey", "email", "name"]
 
-export const EXCLUDE_CONTACT_PROPS = []
+export const EXCLUDE_JOB_PROPS = ["status", "user"]
 
-export const EXCLUDE_USER_PROPS = ["encryptedKey", "email", "name",]
+export const EXCLUDE_CONTACT_PROPS = ["user"]
 
-export const EXCLUDE_INTERVIEW_PROPS = ["type"]
+export const EXCLUDE_INTERVIEW_PROPS = ["type", "user"]
