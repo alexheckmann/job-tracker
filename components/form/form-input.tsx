038e282a7425787dcf0c02824a793b2074b9e50e
Alex Heckmann <ah@ecommunications.de>
2024-06-27 11:22:56 +0200

    feat: add conditional wrapper to form input if label is available

--- components/form/form-input.tsx
+++ components/form/form-input.tsx
@@ -1,6 +1,7 @@
 import {FormControl, FormItem, FormLabel} from "@/components/ui/form";
 import {Input} from "@/components/ui/input";
-import {HTMLAttributes} from "react";
+import {HTMLAttributes, ReactElement} from "react";
+import {ConditionalWrapper} from "@/components/conditional-wrapper";
 
 interface FormInputProps extends HTMLAttributes<HTMLInputElement> {
     labelName?: string,
@@ -25,21 +26,25 @@ export function FormInput({
 
     return (
         <FormItem>
-            <div className="flex flex-col gap-2">
-                <div className={"flex flex-row items-end justify-between"}>
-                    {labelName &&
-                        <FormLabel>
-                            {labelName}
-                        </FormLabel>}
-                    {required &&
-                        <span className={"select-none text-sm font-light"}>
+            <ConditionalWrapper condition={!!labelName} wrapper={(children: ReactElement) => (
+                <div className="flex flex-col gap-2">
+                    <div className={"flex flex-row items-end justify-between"}>
+                        {labelName &&
+                            <FormLabel>
+                                {labelName}
+                            </FormLabel>}
+                        {required &&
+                            <span className={"select-none text-sm font-light"}>
                             Required
                         </span>}
+                    </div>
+                    {children}
                 </div>
+            )}>
                 <FormControl>
                     <Input {...props} placeholder={placeholder} {...field}/>
                 </FormControl>
-            </div>
+            </ConditionalWrapper>
         </FormItem>
     )
 }
