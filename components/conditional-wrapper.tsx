a71ee6f933310561d40aced9662dca3f3487871b
Alex Heckmann <ah@ecommunications.de>
2024-06-27 11:21:39 +0200

    refactor: improve type safety of conditional wrapper

--- components/conditional-wrapper.tsx
+++ components/conditional-wrapper.tsx
@@ -1,3 +1,11 @@
-// @ts-ignore
-export const ConditionalWrapper = ({condition, wrapper, children}) =>
+import {ReactElement} from "react";
+
+/**
+ * ConditionalWrapper component is a higher order component that wraps children with a wrapper component if a condition is met.
+ * @param condition the condition to be met
+ * @param wrapper the wrapper component(s)
+ * @param children the children to be wrapped
+ * @constructor ConditionalWrapper component
+ */
+export const ConditionalWrapper = ({condition, wrapper, children}: { condition: boolean, wrapper: (children: ReactElement) => ReactElement, children: ReactElement }) =>
     condition ? wrapper(children) : children;
