d999c4999d2b4b5a91ca9e1363aeca8687b4074b
Alex Heckmann <ah@ecommunications.de>
2024-07-12 12:34:10 +0200

    refactor(notes): use css truncation of text overflow for company entries

--- app/notes/company-entry.tsx
+++ app/notes/company-entry.tsx
@@ -2,7 +2,6 @@ import {HTMLAttributes, useState} from "react";
 import {Badge} from "@/components/ui/badge";
 import {cn} from "@/lib/utils";
 import {copyToClipboard} from "@/lib/copy-to-clipboard";
-import {truncateString} from "@/lib/truncate-string";
 import {RemoveButton} from "@/app/settings/remove-button";
 import {Button} from "@/components/ui/button";
 import {CopyIcon, GlobalIcon} from "@/components/icons";
@@ -22,13 +21,15 @@ export function CompanyEntry({entry, removeEntryFunction, isPendingRemoval}: Com
 
     return (
         <Badge variant={"outline"} key={entry.companyName}
-               className={cn("flex items-center align-middle h-6 space-x-4 gap-2 justify-between cursor-pointer max-w-[200px] text-nowrap", entry.isFavorite && "border-ternary")}
+               className={cn("flex items-center align-middle h-6 gap-2 justify-between cursor-pointer max-w-[200px] text-nowrap", entry.isFavorite && "border-ternary")}
                onClick={() => copyToClipboard(entry.companyName)}
                onMouseEnter={() => setShowRemoveButton(true)}
                onMouseLeave={() => setShowRemoveButton(false)}
         >
             {entry.location === "Worldwide" && <GlobalIcon className={"h-3 w-3 text-muted-foreground"}/>}
-            {truncateString(entry.companyName, companyEntryMaxLength)}
+            <span className={"space-x-0 max-w-[180px] truncate"}>
+                {entry.companyName}
+            </span>
             {showRemoveButton ?
                 <RemoveButton disabled={isPendingRemoval}
                               onClick={() => removeEntryFunction(entry)}/> :
