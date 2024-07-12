94c4a81c098037c59c1fd6aed1cc65cc2a641406
Alex Heckmann <ah@ecommunications.de>
2024-07-12 12:21:45 +0200

    docs: add documentation to copy-to-clipboard.ts

--- lib/copy-to-clipboard.ts
+++ lib/copy-to-clipboard.ts
@@ -1,16 +1,21 @@
 import {toast} from "@/components/ui/use-toast";
 
-export function copyToClipboard(text: string) {
-    navigator.clipboard.writeText(text).then(function () {
+/**
+ * Copy the given text to the clipboard.
+ * @param text The text to copy to the clipboard.
+ * @returns void
+ */
+export function copyToClipboard(text: string): void {
+    navigator.clipboard.writeText(text).then(function (): void {
         toast({
             title: "Copied to clipboard",
             description: "The text was copied to your clipboard.",
             variant: "default"
         })
-    }, function (err) {
+    }, function (err): void {
         toast({
             title: "Error",
-            description: "An error occurred while copying the text to your clipboard.",
+            description: `An error occurred while copying the text to your clipboard.: ${err}`,
             variant: "destructive"
         })
     });
