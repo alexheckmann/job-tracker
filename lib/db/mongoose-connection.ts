e9b693f134128782a0cbf18e0cd57c10c9d832f2
Alex Heckmann <ah@ecommunications.de>
2024-07-12 13:01:42 +0200

    docs(db): change documentation for mongoose-connection.ts

--- lib/db/mongoose-connection.ts
+++ lib/db/mongoose-connection.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 import {DB_NAME} from "@/lib/db/db-constants";
 
-const mongoosePromise = mongoose.connect(process.env.MONGODB_URI || "", {
-    dbName: DB_NAME,
-})
-
 /**
  * Exports a module-scoped promise for using Mongoose as the database interaction object. Needs to be awaited before
  * using Mongoose.
  */
+const mongoosePromise = mongoose.connect(process.env.MONGODB_URI || "", {
+    dbName: DB_NAME,
+})
+
 export default mongoosePromise
